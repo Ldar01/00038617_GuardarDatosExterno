bd0659507c330929eb714c05962fbe2137617015
Luisa Arevalo <00038617@uca.edu.sv>
2019-04-25 15:53:02 -0600

    Lo que he logrado avanzar

---
+++ app/src/main/java/com/example/a00038617_tareaalmacenamiento/MainActivity.kt
@@ -0,0 +1,157 @@
+package com.example.a00038617_tareaalmacenamiento
+
+import android.Manifest
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import android.Manifest.permission
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.content.pm.PackageManager
+import android.os.Environment
+import android.support.v4.app.ActivityCompat
+import android.widget.Toast
+import jdk.nashorn.internal.runtime.ScriptingFunctions.readLine
+import android.os.Environment.getExternalStorageDirectory
+import android.util.Log
+import android.util.Log.*
+import sun.text.normalizer.UTF16.append
+import java.io.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_ID_READ_PERMISSION = 100
+    private val REQUEST_ID_WRITE_PERMISSION = 200
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        boton_guardar.setOnClickListener {
+
+        }
+
+    }
+
+    fun askPermissionAndWriteFile() {
+        val canWrite = this.askPermission(REQUEST_ID_WRITE_PERMISSION,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        //
+        if (canWrite) {
+            this.writeFile()
+        }
+    }
+
+    fun askPermissionAndReadFile() {
+        val canRead = this.askPermission(REQUEST_ID_READ_PERMISSION,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+        //
+        if (canRead) {
+            this.readFile()
+        }
+    }
+
+    fun askPermission(requestId: Int, permissionName: String): Boolean {
+        if (android.os.Build.VERSION.SDK_INT >= 23) {
+
+            // Check if we have permission
+            val permission = ActivityCompat.checkSelfPermission(this, permissionName)
+
+
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                // If don't have permission so prompt the user.
+                this.requestPermissions(
+                        arrayOf(permissionName),
+                        requestId
+                )
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun writeFile() {
+
+        val extStore = Environment.getExternalStorageDirectory()
+        // ==> /storage/emulated/0/note.txt
+        val path = extStore.getAbsolutePath() + "/" + fileName
+        i("ExternalStorageDemo", "Save to: $path")
+
+        val datos = data.getText().toString()
+
+        try {
+            val myFile = File(path)
+            myFile.createNewFile()
+            val fOut = FileOutputStream(myFile)
+            val myOutWriter = OutputStreamWriter(fOut)
+            myOutWriter.append(datos)
+            myOutWriter.close()
+            fOut.close()
+
+            Toast.makeText(applicationContext, fileName + " saved", Toast.LENGTH_LONG).show()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        //
+        // Note: If request is cancelled, the result arrays are empty.
+        if (grantResults.size > 0) {
+            when (requestCode) {
+                REQUEST_ID_READ_PERMISSION -> {
+                    run {
+                        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                            readFile()
+                        }
+                    }
+                    run {
+                        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                            writeFile()
+                        }
+                    }
+                }
+                REQUEST_ID_WRITE_PERMISSION -> {
+                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        writeFile()
+                    }
+                }
+            }
+        } else {
+            Toast.makeText(applicationContext, "Permission Cancelled!", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun readFile() {
+
+        val extStore = Environment.getExternalStorageDirectory()
+        // ==> /storage/emulated/0/note.txt
+        val path = extStore.getAbsolutePath() + "/" + fileName
+        i("ExternalStorageDemo", "Read file: $path")
+
+        var linea = ""
+        var fileContent = ""
+        try {
+            val myFile = File(path)
+            val fIn = FileInputStream(myFile)
+            val myReader = BufferedReader(
+                    InputStreamReader(fIn))
+
+            while ((linea = myReader.readLine()) != null) {
+                fileContent += s + "\n"
+            }
+            myReader.close()
+
+            this.contenido.setText(fileContent)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        Toast.makeText(applicationContext, fileContent, Toast.LENGTH_LONG).show()
+    }
+}
